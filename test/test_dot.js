a4282642bb695f6f2b7f419bebced62732c84ea0
drewlab <drew@dictadata.org>
2024-07-11 15:10:57 -0500

    refactor args for dot.get/set

--- test/test_dot.js
+++ test/test_dot.js
@@ -9,24 +9,24 @@ let obj = {
   }
 };
 
-console.log("t1 = " + dot.get("t1", obj));
-console.log("baz = " + dot.get("foo.bar.baz", obj));
+console.log("t1 = " + dot.get(obj, "t1"));
+console.log("baz = " + dot.get(obj, "foo.bar.baz"));
 
-console.log("biz = " + dot.get("foo.bar.biz", obj));
-console.log("set: " + dot.set("foo.bar.biz", obj, "world"));
-console.log("biz = " + dot.get("foo.bar.biz", obj));
+console.log("biz = " + dot.get(obj, "foo.bar.biz"));
+console.log("set: " + dot.set(obj, "foo.bar.biz", "world"));
+console.log("biz = " + dot.get(obj, "foo.bar.biz"));
 
-console.log("set: " + dot.set("t2", obj, "bottom"));
-console.log("t2 = " + dot.get("t2", obj));
+console.log("set: " + dot.set(obj, "t2", "bottom"));
+console.log("t2 = " + dot.get(obj, "t2"));
 
-console.log("set: " + dot.set("foo.level.expand", obj, true));
-console.log("expand = " + dot.get("foo.level.expand", obj));
-console.log("set: " + dot.set("foo.level", obj, "II"));
-console.log("expand = " + dot.get("foo.level.expand", obj));
+console.log("set: " + dot.set(obj, "foo.level.expand", true));
+console.log("expand = " + dot.get(obj, "foo.level.expand"));
+console.log("set: " + dot.set(obj, "foo.level", "II"));
+console.log("expand = " + dot.get(obj, "foo.level.expand"));
 
 let slop = "stringy";
-console.log("stray = " + dot.get("foo.stray", slop));
-console.log("set = " + dot.set("foo.stray", slop, "drip"));
+console.log("stray = " + dot.get(slop, "foo.stray"));
+console.log("set = " + dot.set(slop, "foo.stray", "drip"));
 
 console.log(JSON.stringify(obj,null,2));
 
@@ -69,15 +69,15 @@ let obj2 = {
 };
 
 console.log("");
-console.log("locus = " + dot.get("smt.locus", obj2));
-console.log("fields = " + dot.get("fields", obj2));
-console.log("fields2 = " + dot.get("fields.name=field2.fields", obj2));
-console.log("fld2 = " + dot.get("fields.name=field2.fields.name=fld2.type", obj2));
+console.log("locus = " + dot.get(obj2, "smt.locus"));
+console.log("fields = " + dot.get(obj2, "fields"));
+console.log("fields2 = " + dot.get(obj2, "fields.name=field2.fields"));
+console.log("fld2 = " + dot.get(obj2, "fields.name=field2.fields.name=fld2.type"));
 
-console.log("set description = ", dot.set("description", obj2, "this is a new description"));
-console.log("set source = " + dot.set("source", obj2, "a source reference"));
-console.log("set field2 description = " + dot.set("fields.name=field2.description", obj2, "field description"));
-console.log("set field4 = " + dot.set("fields.field4", obj2, { "name": "field4", "type": "boolean" }));
-console.log("set fld3 = " + dot.set("fields.name=field2.fields.name=fld3", obj2, { "name": "fld3", "type": "boolean" }));
+console.log("set description = ", dot.set(obj2, "description", "this is a new description"));
+console.log("set source = " + dot.set(obj2, "source", "a source reference"));
+console.log("set field2 description = " + dot.set(obj2, "fields.name=field2.description", "field description"));
+console.log("set field4 = " + dot.set({ "name": "field4", "fields.field4", obj2, "type": "boolean" }));
+console.log("set fld3 = " + dot.set({ "name": "fld3", "fields.name=field2.fields.name=fld3", obj2, "type": "boolean" }));
 
 console.log(JSON.stringify(obj2,null,2));
